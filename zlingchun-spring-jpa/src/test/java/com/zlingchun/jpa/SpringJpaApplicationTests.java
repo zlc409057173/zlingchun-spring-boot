b19b0ee41005b49362f71a602e32715593011222
zlc409057173 <409057173@qq.com>
2022-05-28 12:02:37 +0800

    init spring boot jpa component

---
+++ zlingchun-spring-jpa/src/test/java/com/zlingchun/jpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlingchun.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

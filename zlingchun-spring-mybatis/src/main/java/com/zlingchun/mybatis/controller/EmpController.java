e837652c3acbe1bb3676ba11db5ce4572b312331
zlc409057173 <409057173@qq.com>
2022-06-29 17:13:22 +0800

    PageHelper 分页插件

--- zlingchun-spring-mybatis/src/main/java/com/zlingchun/mybatis/controller/EmpController.java
+++ zlingchun-spring-mybatis/src/main/java/com/zlingchun/mybatis/controller/EmpController.java
@@ -1,5 +1,7 @@
 package com.zlingchun.mybatis.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zlingchun.mybatis.entity.po.Emp;
 import com.zlingchun.mybatis.service.EmpService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +25,11 @@ public class EmpController {
     EmpService empServiceImpl;
 
     @GetMapping
-    public List<Emp> getEmps(@RequestParam(value = "empName") String empName){
-        return empServiceImpl.findEmps(empName);
+    public PageInfo<Emp> getEmps(@RequestParam(value = "empName") String empName, @RequestParam("pageNum") Integer pageNum, @RequestParam("pageSize") Integer pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        List<Emp> emps = empServiceImpl.findEmps(empName);
+        PageInfo<Emp> info = new PageInfo<>(emps);
+        return info;
     }
 
 }
